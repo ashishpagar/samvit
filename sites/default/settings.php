f6f97e71cc546864aa4acef7ff5706ba69dde270
ashishpagar <ashishpagar@gmail.com>
2016-02-10 23:35:58 -0500

    Moved Settings to new location

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -717,3 +717,7 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 # if (file_exists(__DIR__ . '/settings.local.php')) {
 #   include __DIR__ . '/settings.local.php';
 # }
+
+ if (file_exists('/var/www/site-php/samvit/samvit.settings.inc')) {
+   include '/var/www/site-php/samvit/samvit.settings.inc';
+ }
